eebd955346d4967205a55674da82a99779f3303a
francescacosta <francescacosta@Francescas-iMac-2.local>
2017-07-15 15:37:32 +1000

    create function to add todo to ul

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 var add = document.getElementsByClassName('add')[0];
 var inputBox = document.getElementsByClassName('input-box')[0];
-
+var toDo = document.getElementsByClassName('toDo')[0];
 //Functions
 var toggleInput = function () {
   if (inputBox.style.display === "" || inputBox.style.display === "none") {
@@ -12,4 +12,11 @@ var toggleInput = function () {
 
 add.onclick = function() {
   toggleInput();
+}
+
+var addItem = function(e) {
+  if (e.keyCode === 13) {
+    toDo.innerHTML += `<li> ${inputBox.value} </li>`;
+    inputBox.value = '';
+  }
 }
